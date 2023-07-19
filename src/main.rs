fa3142d06a229e78f26dad1ed90d9bd6d4832755
qxw_windows <qiangxinwei@outlook.com>
2023-07-19 14:47:19 +0000

    design the ast

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use lalrpop_util::lalrpop_mod;
 use std::env::args;
 use std::fs::read_to_string;
 use std::io::Result;
+mod ast;
 
 // 引用 lalrpop 生成的解析器
 // 因为我们刚刚创建了 sysy.lalrpop, 所以模块名是 sysy
@@ -15,14 +16,14 @@ fn main() -> Result<()> {
     let input = args.next().unwrap();
     args.next();
     let output = args.next().unwrap();
-    
+
     // 读取输入文件
     let input = read_to_string(input)?;
-    
+
     // 调用 lalrpop 生成的 parser 解析输入文件
     let ast = sysy::CompUnitParser::new().parse(&input).unwrap();
-    
+
     // 输出解析得到的 AST
-    println!("{}", ast);
+    println!("{:#?}", ast);
     Ok(())
 }
