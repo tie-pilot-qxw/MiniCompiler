06a6f66140330bcc59bf9589cb6aceff3a4272dd
qxw_windows <qiangxinwei@outlook.com>
2023-07-25 23:58:28 +0800

    math expression -koopa

--- src/ast.rs
+++ src/ast.rs
@@ -2,6 +2,11 @@ use std::{io::Write, sync::atomic::AtomicUsize};
 
 static VAR_NAME: AtomicUsize = AtomicUsize::new(0);
 
+fn gen_var_name() -> String {
+    let id = VAR_NAME.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+    format!("%{}", id.to_string())
+}
+
 #[derive(Debug)]
 pub struct CompUnit {
     pub func_def: FuncDef,
@@ -66,13 +71,90 @@ impl Stmt {
 
 #[derive(Debug)]
 pub struct Exp {
-    pub unary_exp: UnaryExp,
+    pub add_exp: AddExp,
 }
 
 impl Exp {
     fn generate(&self, f: &mut Vec<u8>) -> String {
-        self.unary_exp.generate(f)
+        self.add_exp.generate(f)
     }
+}
+
+#[derive(Debug)]
+pub enum AddExp {
+    MulExp(MulExp),
+    AddExp(Box<AddExp>, AddSign, MulExp),
+}
+
+impl AddExp {
+    fn generate(&self, f: &mut Vec<u8>) -> String {
+        match self {
+            Self::MulExp(mul_exp) => mul_exp.generate(f),
+
+            Self::AddExp(add_exp, sign, mul_exp) => {
+                let v1 = add_exp.generate(f);
+                let v2 = mul_exp.generate(f);
+                let output_name = gen_var_name();
+
+                match sign {
+                    AddSign::Add => {
+                        writeln!(f, "    {} = add {}, {}", output_name, v1, v2).unwrap();
+                    }
+
+                    AddSign::Sub => {
+                        writeln!(f, "    {} = sub {}, {}", output_name, v1, v2).unwrap();
+                    }
+                }
+                output_name
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum AddSign {
+    Add,
+    Sub,
+}
+
+#[derive(Debug)]
+pub enum MulExp {
+    UnaryExp(UnaryExp),
+    MulExp(Box<MulExp>, MulSign, UnaryExp),
+}
+
+impl MulExp {
+    fn generate(&self, f: &mut Vec<u8>) -> String {
+        match self {
+            Self::MulExp(mul_exp, sign, unary_exp) => {
+                let v1 = mul_exp.generate(f);
+                let v2 = unary_exp.generate(f);
+                let output_name = gen_var_name();
+
+                match sign {
+                    MulSign::Div => {
+                        writeln!(f, "    {} = div {}, {}", output_name, v1, v2).unwrap();
+                    }
+                    MulSign::Mod => {
+                        writeln!(f, "    {} = mod {}, {}", output_name, v1, v2).unwrap();
+                    }
+                    MulSign::Mul => {
+                        writeln!(f, "    {} = mul {}, {}", output_name, v1, v2).unwrap();
+                    }
+                }
+                output_name
+            }
+
+            Self::UnaryExp(unary_exp) => unary_exp.generate(f),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum MulSign {
+    Mul,
+    Div,
+    Mod,
 }
 
 #[derive(Debug)]
@@ -84,8 +166,8 @@ pub enum PrimaryExp {
 impl PrimaryExp {
     fn generate(&self, f: &mut Vec<u8>) -> String {
         match self {
-            PrimaryExp::Exp(exp) => exp.generate(f),
-            PrimaryExp::Number(num) => num.generate().to_string(),
+            Self::Exp(exp) => exp.generate(f),
+            Self::Number(num) => num.generate().to_string(),
         }
     }
 }
@@ -113,8 +195,7 @@ impl UnaryExp {
 
             Self::Unary(op, u_exp) => {
                 let input_name = u_exp.generate(f);
-                let id = VAR_NAME.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
-                let output_name = format!("%{}", id.to_string());
+                let output_name = gen_var_name();
                 match op {
                     UnaryOp::Bang => {
                         writeln!(f, "    {} = eq {}, 0", output_name, input_name).unwrap();
